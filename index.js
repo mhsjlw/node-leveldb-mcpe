d0d9598e438e39c1be5ebdebbba817a4a10c1347
mhsjlw <mhsjlw@users.noreply.github.com>
2016-04-10 09:48:33 -0400

    attempt to prevent segfaults, fix delete function and update gitignore

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function generateKey(x,z,type) {
-  var buf = new Buffer(9);
-  buf.writeUInt32LE(x, 0);
-  buf.writeUInt32LE(z, 4);
-  buf.writeInt8(type, 8);
-  return buf;
-}
+// function generateKey(x,z,type) {
+//   var buf = new Buffer(9);
+//   buf.writeUInt32LE(x, 0);
+//   buf.writeUInt32LE(z, 4);
+//   buf.writeInt8(type, 8);
+//   return buf;
+// }
 
-const addon = require('./build/Release/addon');
+var addon = require('bindings')('addon.node');
 
-console.log(addon.open("db"));
-console.log(addon.put("key", 3, "why", 3));
-console.log(addon.get("key", 3));
-//console.log(addon.delete("key", 3));
+console.log(addon.open("test"));
+//console.log(addon.put("asd", 3, "value", 5));
+//console.log(addon.get("asd", 3));
+//console.log(addon.delete("asd", 3));
 console.log(addon.close());
